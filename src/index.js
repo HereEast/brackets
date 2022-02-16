a44b7f217be4ff775cc3f75d96a4ba25278638e3
Here East <nastiapiven@gmail.com>
2022-02-16 23:43:23 +0100

    fead: finished brackets

--- src/index.js
+++ src/index.js
@@ -1,36 +1,67 @@
+// module.exports = function check(str, bracketsConfig) {
+//   let openBrackets = [];
+//   let bracketPairs = {};
+
+//   for(let i = 0; i < bracketsConfig.length; i++) {
+//     openBrackets.push(bracketsConfig[i][0]);
+
+//       let key = bracketsConfig[i][1];
+//       bracketPairs[key] = bracketsConfig[i][0];
+//   }
+
+//   console.log(openBrackets);
+//   console.log(bracketPairs);
+
+//   let stack = [];
+
+//   for(let i = 0; i < str.length; i++) {
+//       let currentSymbol = str[i];
+
+//       if(openBrackets.includes(currentSymbol)) {
+//           stack.push(currentSymbol);
+//       } else {
+//           if(stack.length == 0) {
+//               return false;
+//           }
+
+//           let topElement = stack[stack.length - 1];
+
+//           if(bracketPairs[currentSymbol] === topElement) {
+//               stack.pop();
+//           } else {
+//               return false;
+//           }
+//       }
+//   }
+
+//   return stack.length === 0;
+// }
+
 module.exports = function check(str, bracketsConfig) {
-  let openBrackets = [];
-  let bracketPairs = {};
+   
+    let brackets = bracketsConfig.join('').replace(/,/g, ''); // join: '(,)|,|'
+    console.log(brackets);
+    console.log(str);
+    let stack = [];
 
-  for(let i = 0; i < bracketsConfig.length; i++) {
-    openBrackets.push(bracketsConfig[i][0]);
+    for (let bracket of str) {
+        let bracketsIndex = brackets.indexOf(bracket);
 
-      let key = bracketsConfig[i][1];
-      bracketPairs[key] = bracketsConfig[i][0];
-  }
+        if (bracketsIndex % 2 === 0) { 
+            if (bracket === brackets[bracketsIndex + 1] && stack[stack.length - 1] === bracketsIndex) {
+                stack.pop();
+            } else if (bracket === brackets[bracketsIndex + 1] && stack[stack.length - 1] !== bracketsIndex) {
+                stack.push(bracketsIndex)
+            } else {
+                stack.push(bracketsIndex)
+            }
+        } else {
+            if (stack.pop() !== bracketsIndex - 1) {
+                  return false;
+            }
+        }
+          
+    }
 
-  let stack = [];
-
-  for(let i = 0; i < str.length; i++) {
-      let currentSymbol = str[i];
-
-      if(openBrackets.includes(currentSymbol)) {
-          stack.push(currentSymbol);
-      } else {
-          if(stack.length == 0) {
-              return false;
-          }
-
-          let topElement = stack[stack.length - 1];
-
-          if(bracketPairs[currentSymbol] === topElement) {
-              stack.pop();
-          } else {
-              return false;
-          }
-      }
-  }
-
-  console.log(openBrackets);
-  return stack.length === 0;
+    return stack.length === 0;
 }
