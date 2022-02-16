2e01cdeefa1ae704a330cd94bc20542665a5511e
Here East <nastiapiven@gmail.com>
2022-02-16 21:28:28 +0100

    feat: added brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function check(str, bracketsConfig) {
-  return true;
+  let openBrackets = [];
+  let bracketPairs = {};
+
+  for(let i = 0; i < bracketsConfig.length; i++) {
+    openBrackets.push(bracketsConfig[i][0]);
+
+      let key = bracketsConfig[i][1];
+      bracketPairs[key] = bracketsConfig[i][0];
+  }
+
+  let stack = [];
+
+  for(let i = 0; i < str.length; i++) {
+      let currentSymbol = str[i];
+
+      if(openBrackets.includes(currentSymbol)) {
+          stack.push(currentSymbol);
+      } else {
+          if(stack.length == 0) {
+              return false;
+          }
+
+          let topElement = stack[stack.length - 1];
+
+          if(bracketPairs[currentSymbol] === topElement) {
+              stack.pop();
+          } else {
+              return false;
+          }
+      }
+  }
+
+  console.log(openBrackets);
+  return stack.length === 0;
 }
